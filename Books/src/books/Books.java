61705c7ccc5fe8408dbc601cd66c134be84754e9
hoang121160 <danghoang121101@gmail.com>
2023-11-09 16:08:40 +0700

    demo

--- Books/src/books/Books.java
+++ Books/src/books/Books.java
@@ -14,7 +14,7 @@ public class Books {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello");
     }
     
 }
