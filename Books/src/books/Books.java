cc6f750cd7b2f9c2993520c29ff228918658b831
hoang121160 <danghoang121101@gmail.com>
2023-11-12 00:30:38 +0700

    new

--- Books/src/books/Books.java
+++ Books/src/books/Books.java
@@ -14,10 +14,7 @@ public class Books {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("anh Hoàng");
-        System.out.println("anh Hoàng");
-        System.out.println("test");
+      
     }
     
 }
